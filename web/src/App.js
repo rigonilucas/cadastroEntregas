b4820d18b0886249c30c99feb4163b56600ef4fc
Lucas Rigoni <rigoni.lucas@gmail.com>
2022-05-19 00:52:34 -0300

    .

--- web/src/App.js
+++ web/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import  store  from './store/index';
 import Routes from './routes';
-
+//
 function App() {
   return(
     <Provider store = { store }>
